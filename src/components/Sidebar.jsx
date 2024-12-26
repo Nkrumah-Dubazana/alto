376c3d2d5d26fde833a71c1a2a1068a307d5d2a2
Nkrumah Dubazana <altonkrumah@gmail.com>
2024-12-26 17:09:52 +0200

    feat: enhance Sidebar component with dynamic category selection

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -2,10 +2,12 @@ import { Stack } from "@mui/material";
 
 import { categories } from "../utils/constants";
 
+const selectedCategory = 'New';
+
 const Sidebar = () =>  (
     <Stack direction = "row" sx={{overflow: "auto", height: {sx: 'auto', md: '95%'}, flexDirection: {md: 'column'}, }}>
         {categories.map((category) =>(
-            <button>
+            <button className="category-btn" style = {{background: category.name === selectedCategory && '#FC1503', color: 'white'}}> 
                 <span>{category.icon}</span>
                 <span>{category.name}</span>
             </button>
