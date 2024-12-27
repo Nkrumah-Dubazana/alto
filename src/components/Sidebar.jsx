ef36047d97bbabd89dbf517d89ed13e18aca47d8
Nkrumah Dubazana <altonkrumah@gmail.com>
2024-12-27 09:54:27 +0200

    feat: enhance Sidebar with dynamic styling and update index.css

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -7,9 +7,14 @@ const selectedCategory = 'New';
 const Sidebar = () =>  (
     <Stack direction = "row" sx={{overflow: "auto", height: {sx: 'auto', md: '95%'}, flexDirection: {md: 'column'}, }}>
         {categories.map((category) =>(
-            <button className="category-btn" style = {{background: category.name === selectedCategory && '#FC1503', color: 'white'}}> 
-                <span>{category.icon}</span>
-                <span>{category.name}</span>
+            <button className="category-btn" style = {{background: category.name === selectedCategory && '#00CC00', color: 'white'}} key = {category.name}>
+                
+                <span style={{color: category.name === selectedCategory ? 'white': 'green', 'marginRight': '15px'}}>
+                   {category.icon}
+                </span>
+                <span style={{opacity: category.name === selectedCategory ? '1' : '0.8'}}>
+                   {category.name}
+                </span>
             </button>
         ))}    
     </Stack>
