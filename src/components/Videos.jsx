9431237988163d23e587a87d664c6a08e80e5dbf
Nkrumah Dubazana <altonkrumah@gmail.com>
2024-12-28 10:36:06 +0200

    fix: resolve runtime error in Videos rendering and continue VideoCard development

--- src/components/Videos.jsx
+++ src/components/Videos.jsx
@@ -1,16 +1,18 @@
-import {Stack, Box} from '@mui/material';
-import VideoCard from './VideoCard';
-import ChannelCard from './ChannelCard';
+import { Stack, Box } from '@mui/material';
+import {VideoCard, ChannelCard} from './';
 
-const Videos = ({videos}) => {
+const Videos = ({videos }) => {
+ 
   return (
-    <Stack direction = "row" flexWrap="wrap" justifyContent="start" gap={2}>
-        {videos.map((item, idx) => (<Box key ={idx}>
-            {item.id.videoId && <VideoCard video ={item}/>}
-            {item.id.ChannelId && <ChannelCard video ={item}/>}
-        </Box>))}
+    <Stack direction="row" flexWrap="wrap" justifyContent="start" gap={2}>
+      {videos.map((item, idx) => (
+        <Box key={idx}>
+          {item.id.videoId && <VideoCard video={item} />}
+          {/*{item.id.channelId && <ChannelCard channelDetail={item} />}*/}
+        </Box>
+      ))}
     </Stack>
-  )
-}
+  );
+};
 
-export default Videos
+export default Videos;
