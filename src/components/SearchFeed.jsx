32288a2d30d6a7036b9a006819cf6df541cc8c95
Nkrumah Dubazana <altonkrumah@gmail.com>
2024-12-30 15:11:59 +0200

    feat: enhance SearchBar and integrate search functionality in SearchFeed

--- src/components/SearchFeed.jsx
+++ src/components/SearchFeed.jsx
@@ -1,9 +1,35 @@
-import React from 'react'
+import { useState, useEffect } from 'react';
+import { Box, Typography } from '@mui/material';
+import { useParams } from 'react-router-dom';
+
+import Videos from './Videos';
+import { fetchFromAPI } from '../utils/fetchFromAPI';
 
 const SearchFeed = () => {
-  return (
-    <div>SearchFeed</div>
-  )
-}
+  
+  const [videos, setVideos] = useState([]);
+  const {searchTerm} = useParams();
 
-export default SearchFeed
+  useEffect(() => {
+    fetchFromAPI(`search?part=snippet&q=${searchTerm}`).then((data) =>
+      setVideos(data.items)
+    );
+  }, [searchTerm]);
+
+  return (
+    <Box p={2} sx={{ overflow: 'auto', height: '90vh', flex: 2 }}>
+      <Typography
+        variant="h4"
+        fontWeight="bold"
+        mb={2}
+        sx={{ color: 'white' }}
+      >
+        Search Results for: <span style={{ color: '#00CC00' }}>{searchTerm}</span> Videos
+      </Typography>
+
+      <Videos videos={videos} />
+    </Box>
+  );
+};
+
+export default SearchFeed;
